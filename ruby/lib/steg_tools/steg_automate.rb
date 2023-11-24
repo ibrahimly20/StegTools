c157dcead29ac1938e96e3813b6d60fb5c6df8ee
ibking20 <47895855+ibking20@users.noreply.github.com>
2023-11-24 11:53:21 +0000

    add exiftool searching

--- ruby/lib/steg_tools/steg_automate.rb
+++ ruby/lib/steg_tools/steg_automate.rb
@@ -1,18 +1,20 @@
 require 'safe_shell'
 require_relative 'steg_tools'
 
+CMD = ['strings', 'exiftool']
+
 class StegTools::StegAutomate
-  attr_accessor :search_strings, :options
-  attr_reader :strings_result
+  attr_accessor :options
+  attr_reader :strings_result, :flag
 
   def initialize(options)
     @options = options
   end
 
-  def search_strings
-    @strings_result = SafeShell.execute('strings', @options[:file])
+  def search_strings(command)
+    @strings_result = SafeShell.execute(command, @options[:file])
     unless $?.success?
-      raise SafeShell::CommandFailedException.new("Shell command #{['strings', @options[:file]].inspect} failed with status #{$?}")
+      raise SafeShell::CommandFailedException.new("Shell command #{[command, @options[:file]].inspect} failed with status #{$?}")
     end
   rescue => err
     case err
@@ -32,21 +34,33 @@ class StegTools::StegAutomate
   end
 
   def index_of_delimetter(start)
-    new_string = @strings_result[start..-1]
-    return new_string.index(@options[:pattern]) + start
+    return @strings_result.index(@options[:pattern], start)
   end
 
   def search_for_specific_string
     start = index_of_pattern
     if file_contain_string?
-      @strings_result = @strings_result[start..index_of_delimetter(start)]
+      @flag = @strings_result[start..index_of_delimetter(start)]
     end
   end
 
-  def run
-    search_strings
+  def search(command)
+    search_strings(command)
+    yield if block_given?
     search_for_specific_string
-    puts "[x] flag not found !!!" if @strings_result.nil?
-    puts "[+] the flag is : #{@strings_result}" unless @strings_result.nil?
+    render_response unless @flag.nil?
+  end
+
+  def run
+    CMD.each do |cmd|
+      search(cmd)
+    end
+    puts "[x] flag not found !!!" if @flag.nil?
+    exit
+  end
+
+  def render_response
+    puts "[+] the flag is : #{@flag}"
+    exit
   end
 end
